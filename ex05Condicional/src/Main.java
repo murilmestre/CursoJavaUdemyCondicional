0168946df13a7163c705a93c395166850a795d6f
Murilo Medeiros <muriloomestre@live.com>
2021-10-28 13:02:05 -0300

    "EX05Condicional"

---
+++ ex05Condicional/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        int codigo = sc.nextInt();
+        int quantidade = sc.nextInt();
+        double total;
+        if (codigo ==1){
+        total = quantidade*4.0;
+        }
+       else if (codigo ==2){
+            total = quantidade*4.5;
+        }
+        else if (codigo ==3){
+            total = quantidade*5.00;
+
+        }
+        else if (codigo ==4){
+            total = quantidade*2.00;
+
+        }
+        else{
+            total = quantidade*1.50;
+
+
+        }
+        System.out.printf("Total: R$ %.2f%n",total);
+
+
+
+
+
+        sc.close();
+
+    }
+}
